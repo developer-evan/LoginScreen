070eb8b89626f183e4d6c53b7d9481579938a56b
Evans-254 <71435810+Evans-254@users.noreply.github.com>
2023-05-04 20:58:06 +0300

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,73 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  Image,
+  TouchableOpacity,
+} from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView>
+      <View>
+        <View>
+          <Image source={require("./assets/logo.png")}></Image>
+          <Text>Login Page</Text>
+        </View>
+        <View>
+          <TouchableOpacity>
+            <View>
+              <Image source={require("./assets/logo.png")}></Image>
+              <Text>Login Page</Text>
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <View>
+              <Image source={require("./assets/logo.png")}></Image>
+              <Text>Login Page</Text>
+            </View>
+            <Button
+  onPress={onPressLearnMore}
+  title="Learn More"
+  color="#841584"
+  accessibilityLabel="Learn more about this purple button"
+/>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
+  auto: {
+    backgroundColor: "blue",
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "blue",
+    alignItems: "center",
+    justifyContent: "center",
+    fontWeight: "bold",
+    fontSize: 20,
+    textAlign: "center",
+  },
+  body: {
+    flex: 2,
+    backgroundColor: "red",
+    alignItems: "center",
+    justifyContent: "center",
+    textAlign: "center",
+    width: 413,
+  },
+  body2: {
+    flex: 3,
+    backgroundColor: "green",
+    alignItems: "center",
+    justifyContent: "center",
+    textAlign: "center",
+    width: 413,
   },
 });
